2c7443b89f13c4873c3d58907c02010bfbbcaab2
MainaCal <36132327+MainaCal@users.noreply.github.com>
2022-05-17 12:17:53 +0300

    Add files via upload

---
+++ ofamis-project-client/gatsby-config.js
@@ -0,0 +1,37 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  siteMetadata: {
+    title: `ofamis`,
+    description: `ofamis Project`,
+    author: `@Author`,
+    siteUrl: `https://ofamis.com`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/assets/images/logo.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
